6cd118430a9c36b3aee47036571f0a2b40a87677
Danila <danila_egorenko@icloud.com>
2023-01-08 20:31:55 +0300

    fixed bug - refresh page leads to error

---
+++ src/components/Block.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    props: ['qu']
+}
+</script>
+<template>
+    <div class="block">
+        <hr>
+        <h3>{{ qu.title }}</h3>
+        <v-container class="px-0 answers" fluid>
+            <v-radio-group v-model="radioGroup">
+                <v-radio v-for="(a, i) in qu.vars" :key="i" :label="a"></v-radio>
+            </v-radio-group>
+        </v-container>
+    </div>
+</template>
+<style scoped>
+hr {
+    margin: 10px 0;
+}
+</style>
--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -1,7 +1,15 @@
 <script>
+import Block from '../components/Block.vue'
 export default {
+    components: {
+        Block
+    },
+    mounted() {
+        this.$store.dispatch('loadQuestions')
+    },
     computed: {
         q() {
+            this.$store.dispatch('loadQuestions')
             return this.$store.getters.getQuestionById(this.$route.params.id)
         }
     },
@@ -14,23 +22,54 @@ export default {
 </script>
 
 <template>
-    <button class="back" @click="back">Назад</button>
-    <div class="created_at">{{ q.created_at }}</div>
-    <div v-if="edited_at" class="edited_at">{{ q.edited_at }}</div>
-    <img :src="q.preview" />
-    <h1>{{ q.title }}</h1>
-    <p>Автор: {{ q.author }}</p>
+    <div class="d-flex ma-3 ml-0">
+        <button class="back" @click="back">Назад</button>
+        <h1 class="mx-3">{{ q.title }}</h1>
+    </div>
+    <div class="flex-box-preview-info d-flex">
+        <img :src="q.preview" />
+        <div class="flex-info d-flex">
+            <div class="created_at">{{ 'Созд.: ' + q.created_at }}</div>
+            <div v-if="q.edited_at" class="edited_at">{{ 'Ред.: ' + q.edited_at }}</div>
+            <p>Автор: {{ q.author }}</p>
+        </div>
+    </div>
     <div class="questions">
         <form action="#">
             <div class="row" v-for="(qu, i) in q.questions" :key="i">
-                <div class="block">
-                    <hr>
-                    <h3>{{ qu.title }}</h3>
-                    <div class="answers">
-
-                    </div>
-                </div>
+                <Block :qu="qu" />
             </div>
         </form>
     </div>
 </template>
+<style scoped>
+button {
+    background-color: #0d6efd;
+    color: white;
+    padding: 5px;
+    border-radius: 5px;
+    height: 45px;
+}
+
+img {
+    width: 80%;
+}
+
+.flex-info {
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
+
+.flex-box-preview-info {
+    justify-content: space-around;
+}
+
+.v-selection-control .v-label {
+    margin: 5px;
+}
+
+.v-selection-control__input input {
+    opacity: 1 !important;
+}
+</style>
